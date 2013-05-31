614345c25460b9e2f052f6eaee6c4ee5b9b3b2a6
Jon Sime <jonsime@gmail.com>
2013-05-30 20:00:52 -0400

    base account controller, just a stub for now

---
+++ lib/EVEWeb/Controller/Account.pm
@@ -0,0 +1,44 @@
+package EVEWeb::Controller::Account;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+EVEWeb::Controller::Account - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->stash->{'template'} = 'account/index.tt2';
+}
+
+
+=head1 AUTHOR
+
+Jon Sime,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
