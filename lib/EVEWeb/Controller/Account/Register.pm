bca78157f26e4897bb5e2be5f151bda4c22ab0a0
Jon Sime <jonsime@gmail.com>
2013-06-01 00:52:08 -0400

    basic validation of registration input

--- lib/EVEWeb/Controller/Account/Register.pm
+++ lib/EVEWeb/Controller/Account/Register.pm
@@ -30,7 +30,8 @@ sub index :Path :Args(0) {
 
     $c->stash->{'captcha'} = $captcha->get_html(
         $c->config->{'recaptcha_api_key_public'},
-        undef, undef,
+        (exists $c->stash->{'captcha_error'} ? $c->stash->{'captcha_error'} : undef),
+        undef,
         { theme => 'white' }
     );
 
@@ -47,6 +48,67 @@ On any failures, set flash appropriately and forward to the index method.
 
 sub do :Local :Args(0) {
     my ($self, $c) = @_;
+
+    $c->stash->{'errors'} = [];
+    $c->stash->{'field_errors'} = {};
+
+    foreach my $fld (qw( username email password password_retype )) {
+        if (!$c->request->params->{$fld} or $c->request->params->{$fld} != m{\w}o) {
+            $c->stash->{'field_errors'}{$fld} = 1;
+            $c->stash->{$fld} = '';
+        } else {
+            $c->stash->{$fld} = $c->request->params->{$fld} if grep { $_ eq $fld } qw( username email );
+        }
+    }
+
+    push(@{$c->stash->{'errors'}}, 'Username must be at least 4 characters long.')
+        if $c->stash->{'username'} =~ m{\w}o and length($c->stash->{'username'}) < 4;
+
+    if (keys %{$c->stash->{'field_errors'}} > 0) {
+        push(@{$c->stash->{'errors'}}, 'Required fields are missing.');
+    }
+
+    if ($c->request->params->{'username'} =~ m{[^a-z0-9 _-]}i) {
+        push(@{$c->stash->{'errors'}}, 'Username contains invalid characters. Please limit your username to letters, numbers, spaces, dashes, and underscores.');
+    }
+
+    if ($c->request->params->{'password'} =~ m{\w}o and $c->request->params->{'password_retype'} =~ m{\w}o
+            and $c->request->params->{'password'} ne $c->request->params->{'password_retype'}) {
+        push(@{$c->stash->{'errors'}}, 'Password fields do not match.');
+    }
+
+    my $captcha = Captcha::reCAPTCHA->new();
+
+    my $result = $captcha->check_answer(
+        $c->config->{'recaptcha_api_key_private'},
+        $c->request->address,
+        $c->request->params->{'recaptcha_challenge_field'},
+        $c->request->params->{'recaptcha_response_field'}
+    );
+
+    if ($result->{'is_valid'}) {
+        $c->stash->{'passed_captcha'} = 1;
+    } else {
+        push(@{$c->stash->{'errors'}}, 'Invalid captcha response.');
+        $c->stash->{'recaptcha_error'} = $result->{'error'};
+    }
+
+    if ($c->stash->{'username'} =~ m{\w}o) {
+        my $res = $c->model('DB')->do(q{
+            select user_id
+            from public.users
+            where lower(username) = lower(?)
+        }, $c->stash->{'username'});
+
+        if ($res && $res->next) {
+            push(@{$c->stash->{'errors'}}, 'That username is not available.');
+        }
+    }
+
+    if (@{$c->stash->{'errors'}} > 0) {
+        $c->forward('index');
+        return;
+    }
 }
 
 =head2 verify
