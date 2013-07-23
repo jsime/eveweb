eda125cd3f3fbcb0e0d1599d7c5bdc450315e081
Jon Sime <jonsime@gmail.com>
2013-07-23 00:48:41 -0400

    use new breadcrumbs functionality for the first time with our budding pilots controller and view

--- lib/EVEWeb/Controller/Pilot.pm
+++ lib/EVEWeb/Controller/Pilot.pm
@@ -16,6 +16,12 @@ Catalyst Controller.
 
 =cut
 
+sub auto :Private {
+    my ($self, $c) = @_;
+
+    push(@{$c->stash->{'breadcrumbs'}}, { name => 'Pilots', link => $c->uri_for('/pilot') });
+}
+
 
 =head2 index
 
