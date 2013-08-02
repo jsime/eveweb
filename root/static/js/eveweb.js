481f04ee331d9507933acd5c8fbfd9ebf92657b7
Jon Sime <jonsime@gmail.com>
2013-08-01 20:43:50 -0400

    add support to the countdown for a shortened form (e.g. '18d' instead of '18 Days')

--- root/static/js/eveweb.js
+++ root/static/js/eveweb.js
@@ -4,42 +4,66 @@ function ew_countdown (id) {
     var duration = moment.duration(moment(el.attr('data')) - moment());
     var countdown = '';
 
-    if (duration.years() > 1) {
-        countdown += ' ' + duration.years() + ' Years'
-    } else if (duration.years() > 0) {
-        countdown += ' 1 Year'
-    }
+    if (el.hasClass('short')) {
+        if (duration.years() > 0) {
+            countdown += ' ' + duration.years() + 'y'
+        }
 
-    if (duration.months() > 1) {
-        countdown += ' ' + duration.months() + ' Months'
-    } else if (duration.months() > 0) {
-        countdown += ' 1 Month'
-    }
+        if (duration.months() > 0) {
+            countdown += ' ' + duration.months() + 'mo'
+        }
 
-    if (duration.days() > 1) {
-        countdown += ' ' + duration.days() + ' Days'
-    } else if (duration.days() > 0) {
-        countdown += ' 1 Day'
-    }
+        if (duration.days() > 0) {
+            countdown += ' ' + duration.days() + 'd'
+        }
 
-    if (duration.hours() > 1) {
-        countdown += ' ' + duration.hours() + ' Hours'
-    } else if (duration.hours() > 0) {
-        countdown += ' 1 Hour'
-    }
+        if (duration.hours() > 0) {
+            countdown += ' ' + duration.hours() + 'h'
+        }
 
-    if (duration.minutes() > 1) {
-        countdown += ' ' + duration.minutes() + ' Minutes'
-    } else if (duration.minutes() > 0) {
-        countdown += ' 1 Minute'
-    }
+        if (duration.minutes() > 0) {
+            countdown += ' ' + duration.minutes() + 'm'
+        }
 
-    if (duration.seconds() > 1) {
-        countdown += ' ' + duration.seconds() + ' Seconds'
-    } else if (duration.seconds() > 0) {
-        countdown += ' 1 Second'
+        countdown += ' ' + duration.seconds() + 's'
     } else {
-        countdown += ' 0 Seconds'
+        if (duration.years() > 1) {
+            countdown += ' ' + duration.years() + ' Years'
+        } else if (duration.years() > 0) {
+            countdown += ' 1 Year'
+        }
+
+        if (duration.months() > 1) {
+            countdown += ' ' + duration.months() + ' Months'
+        } else if (duration.months() > 0) {
+            countdown += ' 1 Month'
+        }
+
+        if (duration.days() > 1) {
+            countdown += ' ' + duration.days() + ' Days'
+        } else if (duration.days() > 0) {
+            countdown += ' 1 Day'
+        }
+
+        if (duration.hours() > 1) {
+            countdown += ' ' + duration.hours() + ' Hours'
+        } else if (duration.hours() > 0) {
+            countdown += ' 1 Hour'
+        }
+
+        if (duration.minutes() > 1) {
+            countdown += ' ' + duration.minutes() + ' Minutes'
+        } else if (duration.minutes() > 0) {
+            countdown += ' 1 Minute'
+        }
+
+        if (duration.seconds() > 1) {
+            countdown += ' ' + duration.seconds() + ' Seconds'
+        } else if (duration.seconds() > 0) {
+            countdown += ' 1 Second'
+        } else {
+            countdown += ' 0 Seconds'
+        }
     }
 
     el.html(countdown);
