436b273d75f36c7e8e5cfa9f8980885f2e39b48d
Jon Sime <jonsime@gmail.com>
2013-08-05 21:46:57 -0400

    simplistic user admin controller

--- lib/EVEWeb/Controller/Admin/Users.pm
+++ lib/EVEWeb/Controller/Admin/Users.pm
@@ -24,7 +24,31 @@ Catalyst Controller.
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
-    $c->response->body('Matched EVEWeb::Controller::Admin::Users in Admin::Users.');
+    my $res = $c->model('DB')->do(q{
+        select u.user_id, u.email, u.username, u.verified,
+            to_char(u.created_at at time zone ?, ?) as created_at,
+            to_char(u.updated_at at time zone ?, ?) as updated_at,
+            to_char(u.deleted_at at time zone ?, ?) as deleted_at,
+            count(distinct(k.key_id)) as api_keys,
+            count(distinct(p.pilot_id)) as pilots
+        from public.users u
+            left join eve.api_keys k on (k.user_id = u.user_id)
+            left join eve.pilot_api_keys pk on (pk.key_id = k.key_id)
+            left join eve.pilots p on (p.pilot_id = pk.pilot_id)
+        group by u.user_id, u.email, u.username, u.verified,
+            u.created_at, u.updated_at, u.deleted_at
+        order by u.username asc
+    });
+
+    if ($res) {
+        $c->stash->{'users'} = [];
+
+        while ($res->next) {
+            push(@{$c->stash->{'users'}}, { map { $_ => $res->{$_} } $res->columns });
+        }
+    }
+
+    $c->stash->{'template'} = 'admin/users/index.tt2';
 }
 
 
