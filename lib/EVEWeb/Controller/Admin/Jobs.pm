d19345651cd47b787f3081307667f2263b9f7754
Jon Sime <jonsime@gmail.com>
2013-08-05 21:01:47 -0400

    basic job queue listing

--- lib/EVEWeb/Controller/Admin/Jobs.pm
+++ lib/EVEWeb/Controller/Admin/Jobs.pm
@@ -24,7 +24,25 @@ Catalyst Controller.
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
-    $c->response->body('Matched EVEWeb::Controller::Admin::Jobs in Admin::Jobs.');
+    my $res = $c->model('DB')->do(q{
+        select j.job_id, j.job_type, j.job_key, j.stash, j.run_host, j.run_pid,
+            to_char(j.run_at      at time zone ?, ?) as run_at,
+            to_char(j.started_at  at time zone ?, ?) as started_at,
+            to_char(j.finished_at at time zone ?, ?) as finished_at,
+            to_char(j.created_at  at time zone ?, ?) as created_at
+        from public.jobs j
+        order by j.run_at desc limit 50;
+    }, ($c->stash->{'user'}{'timezone'}, $c->stash->{'user'}{'format_datetime'}) x 4);
+
+    if ($res) {
+        $c->stash->{'jobs'} = [];
+
+        while ($res->next) {
+            push(@{$c->stash->{'jobs'}}, { map { $_ => $res->{$_} } $res->columns });
+        }
+    }
+    
+    $c->stash->{'template'} = 'admin/jobs/index.tt2';
 }
 
 
