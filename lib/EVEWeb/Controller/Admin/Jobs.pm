92b6d53953911a87fb9844fd696678b7b1b51e12
Jon Sime <jonsime@gmail.com>
2013-08-06 21:33:29 -0400

    use pagination on job queries and store pager objects in the stash

--- lib/EVEWeb/Controller/Admin/Jobs.pm
+++ lib/EVEWeb/Controller/Admin/Jobs.pm
@@ -29,9 +29,13 @@ sub auto :Private {
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
-    my $json = JSON->new;
+    my $page_current  = $c->request->params->{'pc'} || 1;
+    my $page_open     = $c->request->params->{'po'} || 1;
+    my $page_finished = $c->request->params->{'pf'} || 1;
 
-    my $res = $c->model('DB')->do(q{
+    my $json = JSON->new->pretty;
+
+    my $res = $c->model('DB')->do({ page => $page_current, per_page => 25 }, q{
         select j.job_id, j.job_type, j.job_key, j.stash, j.run_host, j.run_pid,
             to_char(j.run_at      at time zone ?, ?) as run_at,
             to_char(j.started_at  at time zone ?, ?) as started_at,
@@ -41,21 +45,22 @@ sub index :Path :Args(0) {
         where j.started_at is not null
             and j.finished_at is null
         order by j.started_at asc
-        limit 25
     }, ($c->stash->{'user'}{'timezone'}, $c->stash->{'user'}{'format_datetime'}) x 4);
 
     if ($res) {
+        $c->stash->{'current_jobs_pager'} = $res->pager;
         $c->stash->{'current_jobs'} = [];
 
         while ($res->next) {
             push(@{$c->stash->{'current_jobs'}}, { map { $_ => $res->{$_} } $res->columns });
 
-            $c->stash->{'current_jobs'}[-1]->{'stash'} =
-                $json->pretty->encode($json->decode($c->stash->{'current_jobs'}[-1]->{'stash'}));
+            $c->stash->{'current_jobs'}[-1]{'stash'} =
+                $json->encode($json->decode($c->stash->{'current_jobs'}[-1]{'stash'}));
+            $c->stash->{'current_jobs'}[-1]{'stash'} =~ s{(^\s+|\s+$)}{}ogs;
         }
     }
     
-    $res = $c->model('DB')->do(q{
+    $res = $c->model('DB')->do({ page => $page_open, per_page => 10 }, q{
         select j.job_id, j.job_type, j.job_key, j.stash, j.run_host, j.run_pid,
             to_char(j.run_at      at time zone ?, ?) as run_at,
             to_char(j.started_at  at time zone ?, ?) as started_at,
@@ -64,21 +69,22 @@ sub index :Path :Args(0) {
         from public.jobs j
         where j.started_at is null
         order by j.run_at asc
-        limit 10
     }, ($c->stash->{'user'}{'timezone'}, $c->stash->{'user'}{'format_datetime'}) x 4);
 
     if ($res) {
+        $c->stash->{'open_jobs_pager'} = $res->pager;
         $c->stash->{'open_jobs'} = [];
 
         while ($res->next) {
             push(@{$c->stash->{'open_jobs'}}, { map { $_ => $res->{$_} } $res->columns });
 
-            $c->stash->{'open_jobs'}[-1]->{'stash'} =
-                $json->pretty->encode($json->decode($c->stash->{'open_jobs'}[-1]->{'stash'}));
+            $c->stash->{'open_jobs'}[-1]{'stash'} =
+                $json->encode($json->decode($c->stash->{'open_jobs'}[-1]{'stash'}));
+            $c->stash->{'open_jobs'}[-1]{'stash'} =~ s{(^\s+|\s+$)}{}ogs;
         }
     }
     
-    $res = $c->model('DB')->do(q{
+    $res = $c->model('DB')->do({ page => $page_finished, per_page => 10 }, q{
         select j.job_id, j.job_type, j.job_key, j.stash, j.run_host, j.run_pid,
             to_char(j.run_at      at time zone ?, ?) as run_at,
             to_char(j.started_at  at time zone ?, ?) as started_at,
@@ -87,17 +93,18 @@ sub index :Path :Args(0) {
         from public.jobs j
         where j.finished_at is not null
         order by j.finished_at desc
-        limit 10
     }, ($c->stash->{'user'}{'timezone'}, $c->stash->{'user'}{'format_datetime'}) x 4);
 
     if ($res) {
+        $c->stash->{'finished_jobs_pager'} = $res->pager;
         $c->stash->{'finished_jobs'} = [];
 
         while ($res->next) {
             push(@{$c->stash->{'finished_jobs'}}, { map { $_ => $res->{$_} } $res->columns });
 
-            $c->stash->{'finished_jobs'}[-1]->{'stash'} =
-                $json->pretty->encode($json->decode($c->stash->{'finished_jobs'}[-1]->{'stash'}));
+            $c->stash->{'finished_jobs'}[-1]{'stash'} =
+                $json->encode($json->decode($c->stash->{'finished_jobs'}[-1]{'stash'}));
+            $c->stash->{'finished_jobs'}[-1]{'stash'} =~ s{(^\s+|\s+$)}{}ogs;
         }
     }
     
