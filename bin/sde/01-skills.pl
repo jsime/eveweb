aa8aab8a6bf3365e9de5cb907a8960dc29b8678c
Jon Sime <jonsime@gmail.com>
2013-08-09 21:23:15 -0400

    collect primary/secondary attributes for skills from CCP SDE

--- bin/sde/01-skills.pl
+++ bin/sde/01-skills.pl
@@ -78,9 +78,13 @@ printf("Created: %d / Updated: %d\n", $created, $updated);
 #### SKILLS
 $res = $sde_db->do(q{
     select t.*,
-        coalesce(ta."valueInt", ta."valueFloat", 1) as rank
+        coalesce(ta1."valueInt", ta1."valueFloat", 1) as rank,
+        coalesce(ta2."valueInt", ta2."valueFloat", 165) as primary_attribute_id,
+        coalesce(ta3."valueInt", ta3."valueFloat", 165) as secondary_attribute_id
     from "invTypes" t
-        left join "dgmTypeAttributes" ta on (ta."typeID" = t."typeID" and ta."attributeID" = 275)
+        left join "dgmTypeAttributes" ta1 on (ta1."typeID" = t."typeID" and ta1."attributeID" = 275)
+        left join "dgmTypeAttributes" ta2 on (ta2."typeID" = t."typeID" and ta2."attributeID" = 180)
+        left join "dgmTypeAttributes" ta3 on (ta3."typeID" = t."typeID" and ta3."attributeID" = 181)
     where "groupID" IN (select "groupID" from "invGroups" where "categoryID" = 16)
 });
 
@@ -91,6 +95,17 @@ print "\n------\n";
 
 my @skill_ids; # used later to limit dgmTypeAttributes query to just skills
 
+# CCP has two sets of IDs for character attributes - the chrAttributes ID and the
+# dgmAttributeTypes ID. They are not the same numbers. Maybe as I work more with
+# their schema I'll come to realize its "genius." For now, I am unconvinced.
+my %attr_map = (
+    164 => 2, # Charisma
+    165 => 1, # Intelligence
+    166 => 4, # Memory
+    167 => 3, # Perception
+    168 => 5, # Willpower
+);
+
 ($created, $updated) = (0,0);
 SKILL:
 while ($res->next) {
@@ -100,6 +115,10 @@ while ($res->next) {
     # values. Because the training time makes no difference for these (a real character
     # will never possess these skills), we'll set any non-integer ranks to 1.
     $res->{'rank'} = 1 unless $res->{'rank'} =~ m{^\d+$}o;
+
+    # Map the primary and secondary attribute IDs to the 'correct' values.
+    $res->{'primary_attribute_id'}   = $attr_map{$res->{'primary_attribute_id'}};
+    $res->{'secondary_attribute_id'} = $attr_map{$res->{'secondary_attribute_id'}};
 
     push(@skill_ids, $res->{'typeID'});
 
@@ -112,8 +131,8 @@ while ($res->next) {
         name           => $res->{'typeName'},
         description    => $res->{'description'} || '',
         rank           => $res->{'rank'},
-        primary_attribute_id   => 1, # TODO
-        secondary_attribute_id => 1, # TODO
+        primary_attribute_id   => $res->{'primary_attribute_id'},
+        secondary_attribute_id => $res->{'secondary_attribute_id'},
         published      => ($res->{'published'} ? 1 : 0)
     }, $res->{'typeID'});
 
@@ -129,8 +148,8 @@ while ($res->next) {
             name           => $res->{'typeName'},
             description    => $res->{'description'} || '',
             rank           => $res->{'rank'},
-            primary_attribute_id   => 1, # TODO
-            secondary_attribute_id => 1, # TODO
+            primary_attribute_id   => $res->{'primary_attribute_id'},
+            secondary_attribute_id => $res->{'secondary_attribute_id'},
             published      => ($res->{'published'} ? 1 : 0)
         });
 
