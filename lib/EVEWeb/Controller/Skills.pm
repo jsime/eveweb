ba45fd3db32e2c70cd6001cf3bf3ec09af35fffd
Jon Sime <jonsime@gmail.com>
2013-09-02 23:56:25 -0400

    don't include debugging prints in our commits mkay?

--- lib/EVEWeb/Controller/Skills.pm
+++ lib/EVEWeb/Controller/Skills.pm
@@ -143,9 +143,6 @@ sub skills :PathPart Chained('/') Args(1) {
                     += $res->{'train_seconds'};
             }
         }
-
-use Data::Dumper;
-        print STDERR "\n\n\n" . Dumper($c->stash->{'pilot_skills'}) . "\n\n\n";
     }
 
     push(@{$c->stash->{'breadcrumbs'}},
